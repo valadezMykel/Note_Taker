56fb7702faedfeb1bdf87271192c3ed0ea7ef3bc
Mykel Valadez <valadezmykel@gmail.com>
2020-11-15 12:42:42 -0800

    api/notes .get is working

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,31 +1,32 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs")
+const fs = require("fs");
+const { request } = require("http");
 
 const app = express();
 const PORT = 4400;
 
-app.use(express.static("Develop/public"));
+app.use(express.static("./Develop/public"));
 
 app.get("/notes", function(request, response){
     response.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+
+app.get("/api/notes", function(req, res){
+    fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
+        if(err) throw err;
+        res.json(JSON.parse(data));
+    });
 });
 
 app.get("*", function(req, res){
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/api/notes", function(req, res){
-    console.log("is working")
-    res.send(fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
-        if(err) throw err;
-        console.log(data);
-        return JSON.stringify(data);
-    }));
-});
-
 app.post("/api/notes", function(req, res){
-
+    // takes db, parses, then pushes new object, then puts it back into db
+    let toaSaveNote = req.body;
     res.send(req.body);
 });
 
