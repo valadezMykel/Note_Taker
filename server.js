41af0d40e69abd4462d699f79edd1d8d194bdf0f
Mykel Valadez <valadezmykel@gmail.com>
2020-11-19 18:30:55 -0800

    made the code more dry with async functions

--- server.js
+++ server.js
@@ -2,68 +2,52 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const uniqid = require("uniqid");
+// utilities packages
+const util = require("util");
+// this does callback function and promises
+const readFile = util.promisify(fs.readFile);
 
 const app = express();
-const PORT = process.env.PORT || 4400;
+const PORT = process.env.PORT || 4401;
 
 app.use(express.static("./Develop/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// function dbAccessor(){
-//     let tt;
-//     fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
-//         if(err) throw err;
-//         console.log("in")
-//         tt = JSON.parse(data);
-//     }).then((tt)=>{
-        
-//         return tt;
-//     });
-// };
-
-// function getId(){
-//     id.increment();
-//     let assignId = id.count;
-//     return assignId;
-// };
+async function dbAccessor(){
+    const wait = await readFile("Develop/db/db.json", "utf8")
+    console.log(wait)
+    return JSON.parse(wait);
+};
 
 app.get("/notes", function(request, response){
     response.sendFile(path.join(__dirname, "Develop/public/notes.html"));
 });
 
-app.get("/api/notes", function(req, res){
-    fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
-        if(err) throw err;
-        res.json(JSON.parse(data));
-    });
-    // res.json(dbAccessor());
+app.get("/api/notes", async function(req, res){
+    res.json( await dbAccessor());
 });
 
 app.get("*", function(req, res){
     res.sendFile(path.join(__dirname, "Develop/public/index.html"));
 });
 
-app.post("/api/notes", function(req, res){
+app.post("/api/notes", async function(req, res){
     let saveThisNote = req.body;
     saveThisNote.id = uniqid();
-    
-    let updateThisArr;
 
-    fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
-        if(err) throw err;
-        updateThisArr = JSON.parse(data);
-        updateThisArr.push(saveThisNote);
-        updateThisArr = JSON.stringify(updateThisArr);
-        fs.writeFile("Develop/db/db.json", updateThisArr, (err) => {if(err) throw err});
-        
-    });
+    let updateThisArr = await dbAccessor();
+
+    updateThisArr.push(saveThisNote);
+    updateThisArr = JSON.stringify(updateThisArr);
+    fs.writeFile("Develop/db/db.json", updateThisArr, (err) => {if(err) throw err});
     res.json(saveThisNote);
 });
 
 app.delete("/api/notes/:id", (req, res) => {
     const deleteNoteWithThisId = req.params.id;
     let postDeleteArr = [];
+
     fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
         let dbArr = JSON.parse(data);
 
